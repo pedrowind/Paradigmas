f76dcb854ef700625bbb43b932f0a5522357bb4b
Rafael Marliere de Oliveira <rafaelmarliere033@gmail.com>
2024-11-21 21:34:04 -0300

    feature: battle between heroes

---
+++ src/main/java/paradigmas/theboys/controllers/BatalhaController.java
@@ -0,0 +1,29 @@
+package paradigmas.theboys.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import paradigmas.theboys.dto.BatalhaDTO;
+import paradigmas.theboys.services.BatalhaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/batalha")
+public class BatalhaController {
+
+    @Autowired
+    private BatalhaService batalhaService;
+
+    @PostMapping
+    public ResponseEntity<BatalhaDTO> realizarBatalha(@RequestParam Integer heroi1Id, @RequestParam Integer heroi2Id) {
+        BatalhaDTO batalha = batalhaService.realizarBatalha(heroi1Id, heroi2Id);
+        return ResponseEntity.ok(batalha);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BatalhaDTO>> listarBatalhas() {
+        List<BatalhaDTO> batalhas = batalhaService.listarBatalhas();
+        return ResponseEntity.ok(batalhas);
+    }
+}
